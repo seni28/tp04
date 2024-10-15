17dc2e2b14066b55088dae92378226c02c96a79d
seni28 <ines.cabral--toucheboeuf@student.junia.com>
2024-10-15 11:33:45 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ChromeDinoGame from 'react-chrome-dino';
+
+
+function App() {
+  return (
+    <div className="App">
+        <ChromeDinoGame />
+    </div>
+  );
+}
+
+export default App;
